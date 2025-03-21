7a15216e56820d419158d07b1f3e920af13985b7
jailx365 <battzSHOT11@gmail.com>
2025-03-21 08:20:37 +0800

    Rearranged Scripts folder

---
+++ Runtime/Scripts/Components/OnEnableScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnEnableScript : MonoBehaviour 
+{
+	public UnityEvent onEnable;
+	public UnityEvent onDisable;
+
+    void OnEnable()
+    {
+        onEnable?.Invoke();
+    }
+    void OnDisable()
+    {
+        onDisable?.Invoke();
+    }
+}
