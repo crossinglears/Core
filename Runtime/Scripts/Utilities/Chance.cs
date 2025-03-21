f4d27c401c76bff4d8e9a8dab361aa155a7465a6
jailx365 <battzSHOT11@gmail.com>
2025-03-21 18:53:29 +0800

    Chance Class added

---
+++ Runtime/Scripts/Utilities/Chance.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace CrossingLears
+{
+    public static class Chance
+    {
+        public static bool chance(float chanceTrue) => Random.Range(0, 100f) < chanceTrue;
+
+        public static bool chance(int chanceTrue) => Random.Range(0, 101) < chanceTrue;
+
+        public static bool chance() => Random.value > 0.5f;
+
+        public static int round(float input)
+        {
+            int whole = (int)input;
+            return chance((input - whole) * 100) ? whole + 1 : whole;
+        }
+    }
+}
