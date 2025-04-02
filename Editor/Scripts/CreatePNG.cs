b45effbc70d90585a310858fd14aa4a6aec8263f
jailx365 <battzSHOT11@gmail.com>
2025-04-02 14:53:18 +0800

    Create EmptyPNG added

---
+++ Editor/Scripts/CreatePNG.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace CrossingLearsEditor
+{
+    public class CreateEmptyPNG
+    {
+        [MenuItem("Assets/Create/CrossingLears/Empty PNG", false, 100)]
+        private static void CreatePNG()
+        {
+            string path = GetSelectedPathOrFallback() + "/NewImage.png";
+
+            if (File.Exists(path)) // Avoid overwriting existing files
+            {
+                path = AssetDatabase.GenerateUniqueAssetPath(path);
+            }
+
+            byte[] emptyPNG = GenerateEmptyPNG(256, 256); // Default size
+            File.WriteAllBytes(path, emptyPNG);
+
+            AssetDatabase.Refresh();
+        }
+
+        private static byte[] GenerateEmptyPNG(int width, int height)
+        {
+            Texture2D texture = new Texture2D(width, height);
+            Color clear = new Color(0, 0, 0, 0); // Transparent
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    texture.SetPixel(x, y, clear);
+                }
+            }
+            texture.Apply();
+            return texture.EncodeToPNG();
+        }
+
+        private static string GetSelectedPathOrFallback()
+        {
+            string path = "Assets";
+            foreach (Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path);
+                }
+                break;
+            }
+            return path;
+        }
+    }
+
+}
--- Runtime/Do Not Touch/CL_Init.cs
+++ Runtime/Do Not Touch/CL_Init.cs
@@ -5,7 +5,7 @@ namespace CrossingLears
     public static class GameInitializer
     {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        static void OnGameStart()
+        static void OnSceneStart()
         {
             Debug.Log("Crossing Lears: Game Started!");
         }
