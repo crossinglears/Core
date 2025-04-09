8d4a14136206bf61ca24a71a44f3f8b058c988aa
jailx365 <battzSHOT11@gmail.com>
2025-04-09 12:38:39 +0800

    Moved CL_CommandAttribute to Runtime

--- Editor/CL Window/CommandTab.cs
+++ Editor/CL Window/CommandTab.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using CrossingLears;
 
 namespace CrossingLearsEditor
 {
@@ -276,40 +277,6 @@ namespace CrossingLearsEditor
                 GUILayout.EndHorizontal();
             }
         }
-    }
-
-    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
-    public class CL_CommandAttribute : Attribute
-    {
-        public string Key { get; set; }
-        public MethodType m_type { get; set; } = MethodType.Asset;
-        public bool CanCallAll { get; set; }
-
-        public CL_CommandAttribute(string key, MethodType methodType, bool CanCallAll = false)
-        {
-            Key = key;
-            m_type = methodType;
-            this.CanCallAll = CanCallAll;
-        }
-
-        public CL_CommandAttribute(MethodType methodType, bool CanCallAll = false)
-        {
-            this.CanCallAll = CanCallAll;
-            m_type = methodType;
-        }
-    }
-
-    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
-    public class CL_CommandHolderAttribute : Attribute
-    {
-        public CL_CommandHolderAttribute() { }
-    }
-
-    public enum MethodType
-    {
-        Asset,
-        SceneObject,
-        Static
     }
 }
 
---
+++ Runtime/Attributes/Cl_Command.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace CrossingLears
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class CL_CommandAttribute : Attribute
+    {
+        public string Key { get; set; }
+        public MethodType m_type { get; set; } = MethodType.Asset;
+        public bool CanCallAll { get; set; }
+
+        public CL_CommandAttribute(string key, MethodType methodType, bool CanCallAll = false)
+        {
+            Key = key;
+            m_type = methodType;
+            this.CanCallAll = CanCallAll;
+        }
+
+        public CL_CommandAttribute(MethodType methodType, bool CanCallAll = false)
+        {
+            this.CanCallAll = CanCallAll;
+            m_type = methodType;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
+    public class CL_CommandHolderAttribute : Attribute
+    {
+        public CL_CommandHolderAttribute() { }
+    }
+
+    public enum MethodType
+    {
+        Asset,
+        SceneObject,
+        Static
+    }
+}
