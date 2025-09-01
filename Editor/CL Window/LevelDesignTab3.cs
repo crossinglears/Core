55e038dc8368a22138790f71bd667aca2d121070
jailx365 <battzSHOT11@gmail.com>
2025-09-02 03:29:43 +0800

    Update LevelDesignTab3.cs
    - Now has Global and Local Scale

--- Editor/CL Window/LevelDesignTab3.cs
+++ Editor/CL Window/LevelDesignTab3.cs
@@ -16,7 +16,6 @@ namespace CrossingLearsEditor
                 Transform reference = selectedTransforms[0];
 
                 EditorGUILayout.LabelField("Selected Objects: " + selectedTransforms.Length);
-                // EditorGUILayout.LabelField("Reference Name:", reference.name);
 
                 Object newReference = EditorGUILayout.ObjectField("Reference Object", reference.gameObject, typeof(GameObject), true);
                 if (newReference != null && newReference != reference.gameObject)
@@ -32,14 +31,22 @@ namespace CrossingLearsEditor
                 Vector3 oldLocal = reference.localPosition;
                 Vector3 newLocal = EditorGUILayout.Vector3Field("Local Position", oldLocal);
 
-                if (newWorld != oldWorld || newLocal != oldLocal)
+                Vector3 oldGlobalScale = reference.lossyScale;
+                Vector3 newGlobalScale = EditorGUILayout.Vector3Field("Global Scale", oldGlobalScale);
+
+                Vector3 oldLocalScale = reference.localScale;
+                Vector3 newLocalScale = EditorGUILayout.Vector3Field("Local Scale", oldLocalScale);
+
+                if (newWorld != oldWorld || newLocal != oldLocal || newGlobalScale != oldGlobalScale || newLocalScale != oldLocalScale)
                 {
                     foreach (Transform t in selectedTransforms)
                     {
-                        Undo.RecordObject(t, "Change Transform Position");
+                        Undo.RecordObject(t, "Change Transform");
 
                         Vector3 wp = t.position;
                         Vector3 lp = t.localPosition;
+                        Vector3 gls = t.lossyScale;
+                        Vector3 lcs = t.localScale;
 
                         if (newWorld != oldWorld)
                         {
@@ -54,6 +61,23 @@ namespace CrossingLearsEditor
                             if (newLocal.y != oldLocal.y) lp.y = newLocal.y;
                             if (newLocal.z != oldLocal.z) lp.z = newLocal.z;
                             t.localPosition = lp;
+                        }
+                        else if (newLocalScale != oldLocalScale)
+                        {
+                            if (newLocalScale.x != oldLocalScale.x) lcs.x = newLocalScale.x;
+                            if (newLocalScale.y != oldLocalScale.y) lcs.y = newLocalScale.y;
+                            if (newLocalScale.z != oldLocalScale.z) lcs.z = newLocalScale.z;
+                            t.localScale = lcs;
+                        }
+                        else if (newGlobalScale != oldGlobalScale)
+                        {
+                            Vector3 parentScale = t.parent != null ? t.parent.lossyScale : Vector3.one;
+                            Vector3 adjustedScale = new Vector3(
+                                parentScale.x != 0 ? newGlobalScale.x / parentScale.x : newGlobalScale.x,
+                                parentScale.y != 0 ? newGlobalScale.y / parentScale.y : newGlobalScale.y,
+                                parentScale.z != 0 ? newGlobalScale.z / parentScale.z : newGlobalScale.z
+                            );
+                            t.localScale = adjustedScale;
                         }
 
                         EditorUtility.SetDirty(t);
