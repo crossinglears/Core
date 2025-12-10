8f8f1db5eb66cc02be1646e23a932ccd33a5e8f2
jailx365 <battzSHOT11@gmail.com>
2025-12-10 15:57:27 +0800

    Update PivotTab.cs

--- Editor/CL Window/Windows/PivotTab.cs
+++ Editor/CL Window/Windows/PivotTab.cs
@@ -1,34 +1,3 @@
-// using UnityEngine;
-
-// namespace CrossingLearsEditor
-// {
-//     public class PivotTab : CL_WindowTab
-//     {
-//         public override string TabName => "Pivot";
-
-//         int x, y ,z;
-
-//         public override void DrawContent()
-//         {
-//             string[] par = new string[] { "Left", "Center", "Right" };
-//             string[] par2 = new string[] { "Top", "Center", "Bottom" };
-            
-//             GUILayout.Toolbar(x, par, GUILayout.Width(120), GUILayout.Height(20));
-//             GUILayout.Toolbar(y, par2, GUILayout.Width(120), GUILayout.Height(20));
-//             GUILayout.Toolbar(z, par, GUILayout.Width(120), GUILayout.Height(20));
-            
-//             int pivotSetting = 0;
-//             GUILayout.Toolbar(pivotSetting, new string[] {"Average Position of All Children, Average Bounds Center of All Children"}, GUILayout.Width(120), GUILayout.Height(20));
-
-//             if(GUILayout.Button("Apply Pivot"))
-//             {
-                
-//             }
-//         }
-//     }
-// }
-
-
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -37,12 +6,18 @@ namespace CrossingLearsEditor
 {
     public class PivotTab : CL_WindowTab
     {
+        public enum PivotSetting
+        {
+            AverageBoundsCenter,
+            AveragePosition,
+        }
+
         public override string TabName => "Pivot";
 
         int x = 1;
         int y = 1;
         int z = 1;
-        int pivotSetting = 0;
+        PivotSetting pivotSetting = PivotSetting.AverageBoundsCenter;
 
         public override void DrawContent()
         {
@@ -63,69 +38,118 @@ namespace CrossingLearsEditor
             GUILayout.Label("Z", GUILayout.Width(20));
             z = GUILayout.Toolbar(z, par, GUILayout.Height(20), GUILayout.ExpandWidth(true));
             GUILayout.EndHorizontal();
-                        
+
             GUILayout.BeginHorizontal();
             GUILayout.Label("Pivot Mode", GUILayout.Width(120));
-            pivotSetting = EditorGUILayout.Popup(pivotSetting, new string[] { "Average Bounds Center of All Children", "Average Position of All Children",  });
+            pivotSetting = (PivotSetting)EditorGUILayout.EnumPopup(pivotSetting);
             GUILayout.EndHorizontal();
 
             GUILayout.Space(5);
             if (GUILayout.Button("Apply Pivot", GUILayout.Height(25)))
             {
-                ApplyPivot();
+                ApplyPivot(false);
+            }
+
+            if (GUILayout.Button("Apply Pivot (Include All Descendants)", GUILayout.Height(25)))
+            {
+                ApplyPivot(true);
             }
         }
 
-        void ApplyPivot()
+        void ApplyPivot(bool includeDescendants)
         {
             Transform target = Selection.activeTransform;
             if (target == null) return;
 
-            List<Transform> children = new List<Transform>();
-            for (int i = 0; i < target.childCount; i++)
+            List<Transform> moveChildren = new List<Transform>();
+            List<Transform> calcChildren = new List<Transform>();
+
+            if (includeDescendants)
             {
-                children.Add(target.GetChild(i));
+                Transform[] all = target.GetComponentsInChildren<Transform>();
+                for (int i = 0; i < all.Length; i++)
+                {
+                    if (all[i] != target) calcChildren.Add(all[i]);
+                }
+
+                for (int i = 0; i < target.childCount; i++)
+                {
+                    moveChildren.Add(target.GetChild(i));
+                }
             }
-            if (children.Count == 0) return;
+            else
+            {
+                for (int i = 0; i < target.childCount; i++)
+                {
+                    Transform c = target.GetChild(i);
+                    moveChildren.Add(c);
+                    calcChildren.Add(c);
+                }
+            }
 
-            Vector3 desired;
+            if (calcChildren.Count == 0) return;
 
-            if (pivotSetting == 0) // Average Position
+            Vector3 desired = Vector3.zero;
+
+            if (pivotSetting == PivotSetting.AveragePosition)
             {
                 Vector3 sum = Vector3.zero;
-                foreach (Transform child in children)
+                for (int i = 0; i < calcChildren.Count; i++)
                 {
-                    sum += child.position;
+                    sum += calcChildren[i].position;
                 }
-                desired = sum / children.Count;
+                desired = sum / calcChildren.Count;
             }
-            else // Average Bounds Center
+            else
             {
-                Bounds bounds = new Bounds(children[0].position, Vector3.zero);
-                foreach (Transform child in children)
+                Bounds bounds = new Bounds();
+                bool set = false;
+
+                for (int i = 0; i < calcChildren.Count; i++)
                 {
+                    Transform child = calcChildren[i];
                     Renderer r = child.GetComponent<Renderer>();
+                    Collider c = child.GetComponent<Collider>();
+
                     if (r != null)
                     {
-                        bounds.Encapsulate(r.bounds);
+                        if (!set) { bounds = new Bounds(r.bounds.center, r.bounds.size); set = true; }
+                        else { bounds.Encapsulate(r.bounds); }
+                    }
+                    else if (c != null)
+                    {
+                        if (!set) { bounds = new Bounds(c.bounds.center, c.bounds.size); set = true; }
+                        else { bounds.Encapsulate(c.bounds); }
                     }
                     else
                     {
-                        bounds.Encapsulate(child.position);
+                        if (!set) { bounds = new Bounds(child.position, Vector3.zero); set = true; }
+                        else { bounds.Encapsulate(child.position); }
                     }
                 }
+
                 desired = bounds.center;
             }
 
-            // Adjust pivot according to x, y, z selection
             Bounds finalBounds = new Bounds(desired, Vector3.zero);
-            foreach (Transform child in children)
+            for (int i = 0; i < calcChildren.Count; i++)
             {
+                Transform child = calcChildren[i];
                 Renderer r = child.GetComponent<Renderer>();
+                Collider c = child.GetComponent<Collider>();
+
                 if (r != null)
+                {
                     finalBounds.Encapsulate(r.bounds);
+                }
+                else if (c != null)
+                {
+                    finalBounds.Encapsulate(c.bounds);
+                }
                 else
+                {
                     finalBounds.Encapsulate(child.position);
+                }
             }
 
             float selectX = x == 0 ? finalBounds.min.x : (x == 1 ? finalBounds.center.x : finalBounds.max.x);
@@ -136,9 +160,9 @@ namespace CrossingLearsEditor
 
             Vector3 offset = target.position - desired;
 
-            foreach (Transform child in children)
+            for (int i = 0; i < moveChildren.Count; i++)
             {
-                child.position += offset;
+                moveChildren[i].position += offset;
             }
 
             target.position = desired;
