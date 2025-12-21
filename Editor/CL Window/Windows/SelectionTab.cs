ac3baee755fbbee9331cc7600313ac0478cdd165
jailx365 <battzSHOT11@gmail.com>
2025-12-22 07:55:06 +0800

    Update SelectionTab.cs

--- Editor/CL Window/Windows/SelectionTab.cs
+++ Editor/CL Window/Windows/SelectionTab.cs
@@ -14,192 +14,101 @@ namespace CrossingLearsEditor
         private Vector3 cachedSign;
         private Transform ParentTransform;
 
-        // public override void DrawContent()
-        // {
-        //     if (GUILayout.Button("Clear Selection"))
-        //     {
-        //         selectedObject = null;
-        //         object1 = null;
-        //         object2 = null;
-        //         cachedSign = Vector3.zero;
-        //         Selection.activeGameObject = null;
-        //         return;
-        //     }
-
-        //     selectedObject = Selection.activeGameObject;
-
-        //     EditorGUILayout.ObjectField("Selected Object", selectedObject, typeof(GameObject), true);
-
-        //     GUILayout.BeginHorizontal();
-        //     object1 = EditorGUILayout.ObjectField("Object 1", object1, typeof(GameObject), true) as GameObject;
-        //     if (GUILayout.Button("Select", GUILayout.Width(60)))
-        //     {
-        //         EditorGUIUtility.PingObject(Selection.activeGameObject = object1);
-        //         cachedSign = Vector3.zero;
-        //     }
-        //     GUILayout.EndHorizontal();
-
-        //     GUILayout.BeginHorizontal();
-        //     object2 = EditorGUILayout.ObjectField("Object 2", object2, typeof(GameObject), true) as GameObject;
-        //     if (GUILayout.Button("Select", GUILayout.Width(60)))
-        //     {
-        //         EditorGUIUtility.PingObject(Selection.activeGameObject = object2);
-        //         cachedSign = Vector3.zero;
-        //     }
-        //     GUILayout.EndHorizontal();
-
-        //     GUILayout.Space(10);
-
-        //     EditorGUILayout.BeginHorizontal();
-        //     if (GUILayout.Button("Set Object 1"))
-        //     {
-        //         object1 = selectedObject;
-        //         cachedSign = Vector3.zero;
-        //     }
-        //     if (GUILayout.Button("Set Object 2"))
-        //     {
-        //         object2 = selectedObject;
-        //         cachedSign = Vector3.zero;
-        //     }
-        //     EditorGUILayout.EndHorizontal();
-
-        //     if (object1 != null && object2 != null)
-        //     {
-        //         Vector3 worldDelta = object1.transform.position - object2.transform.position;
-        //         Vector3 localDelta = object1.transform.InverseTransformDirection(worldDelta);
-
-        //         if (cachedSign == Vector3.zero)
-        //         {
-        //             cachedSign = new Vector3(
-        //                 Mathf.Sign(localDelta.x),
-        //                 Mathf.Sign(localDelta.y),
-        //                 Mathf.Sign(localDelta.z)
-        //             );
-        //         }
-
-        //         Vector3 absLocalDelta = new Vector3(
-        //             Mathf.Abs(localDelta.x),
-        //             Mathf.Abs(localDelta.y),
-        //             Mathf.Abs(localDelta.z)
-        //         );
-
-        //         EditorGUI.BeginChangeCheck();
-        //         Vector3 newAbsLocalDelta = EditorGUILayout.Vector3Field("Difference", absLocalDelta);
-        //         if (EditorGUI.EndChangeCheck())
-        //         {
-        //             Vector3 signedLocalDelta = new Vector3(
-        //                 cachedSign.x * newAbsLocalDelta.x,
-        //                 cachedSign.y * newAbsLocalDelta.y,
-        //                 cachedSign.z * newAbsLocalDelta.z
-        //             );
-
-        //             Vector3 newWorldDelta = object1.transform.TransformDirection(signedLocalDelta);
-        //             Undo.RecordObject(object2.transform, "Move Object 2");
-        //             object2.transform.position = object1.transform.position - newWorldDelta;
-        //         }
-
-        //         GUILayout.BeginHorizontal();
-        //         if (GUILayout.Button("Apply difference to Snap"))
-        //         {
-        //             EditorSnapSettings.move = absLocalDelta;
-        //         }
-        //         if (GUILayout.Button("Copy", GUILayout.Width(50)))
-        //         {
-        //             EditorGUIUtility.systemCopyBuffer = absLocalDelta.Vector3ToString();
-        //         }
-        //         GUILayout.EndHorizontal();
-
-        //         if (GUILayout.Button(new GUIContent("Match Bounds Center", "Moves Object 1 so its Renderer bounds center matches Object 2")))
-        //         {
-        //             MatchBoundsCenter(object1.GetComponent<Renderer>(), object2.GetComponent<Renderer>());
-        //         }
-        //     }
-
-        //     ParentSystem();
-        // }
-
-public override void DrawContent()
-{
-    if (GUILayout.Button("Clear Selection"))
-    {
-        selectedObject = null;
-        object1 = null;
-        object2 = null;
-        cachedSign = Vector3.zero;
-        Selection.activeGameObject = null;
-        return;
-    }
-
-    selectedObject = Selection.activeGameObject;
-
-    EditorGUILayout.ObjectField("Selected Object", selectedObject, typeof(GameObject), true);
-
-    GUILayout.BeginHorizontal();
-    object1 = EditorGUILayout.ObjectField("Object 1", object1, typeof(GameObject), true) as GameObject;
-    if (GUILayout.Button("Select", GUILayout.Width(60)))
-    {
-        EditorGUIUtility.PingObject(Selection.activeGameObject = object1);
-    }
-    GUILayout.EndHorizontal();
-
-    GUILayout.BeginHorizontal();
-    object2 = EditorGUILayout.ObjectField("Object 2", object2, typeof(GameObject), true) as GameObject;
-    if (GUILayout.Button("Select", GUILayout.Width(60)))
-    {
-        EditorGUIUtility.PingObject(Selection.activeGameObject = object2);
-    }
-    GUILayout.EndHorizontal();
-
-    GUILayout.Space(10);
-
-    EditorGUILayout.BeginHorizontal();
-    if (GUILayout.Button("Set Object 1"))
-    {
-        object1 = selectedObject;
-    }
-    if (GUILayout.Button("Set Object 2"))
-    {
-        object2 = selectedObject;
-    }
-    EditorGUILayout.EndHorizontal();
-
-    if (object1 != null && object2 != null)
-    {
-        Vector3 worldDelta = object1.transform.position - object2.transform.position;
-        Vector3 localDelta = object1.transform.InverseTransformDirection(worldDelta);
-
-        EditorGUI.BeginChangeCheck();
-        Vector3 newLocalDelta = EditorGUILayout.Vector3Field("Difference", localDelta);
-        if (EditorGUI.EndChangeCheck())
+        public override void DrawContent()
         {
-            Vector3 newWorldDelta = object1.transform.TransformDirection(newLocalDelta);
-            Undo.RecordObject(object2.transform, "Move Object 2");
-            object2.transform.position = object1.transform.position - newWorldDelta;
-        }
+            if (GUILayout.Button("Clear Selection"))
+            {
+                selectedObject = null;
+                object1 = null;
+                object2 = null;
+                cachedSign = Vector3.zero;
+                Selection.activeGameObject = null;
+                return;
+            }
 
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Apply difference to Snap"))
-        {
-            EditorSnapSettings.move = new Vector3(
-                Mathf.Abs(localDelta.x),
-                Mathf.Abs(localDelta.y),
-                Mathf.Abs(localDelta.z)
-            );
-        }
-        if (GUILayout.Button("Copy", GUILayout.Width(50)))
-        {
-            EditorGUIUtility.systemCopyBuffer = localDelta.Vector3ToString();
-        }
-        GUILayout.EndHorizontal();
+            selectedObject = Selection.activeGameObject;
 
-        if (GUILayout.Button(new GUIContent("Match Bounds Center", "Moves Object 1 so its Renderer bounds center matches Object 2")))
-        {
-            MatchBoundsCenter(object1.GetComponent<Renderer>(), object2.GetComponent<Renderer>());
-        }
-    }
+            EditorGUILayout.ObjectField("Selected Object", selectedObject, typeof(GameObject), true);
 
-    ParentSystem();
-}
+            GUILayout.BeginHorizontal();
+            object1 = EditorGUILayout.ObjectField("Object 1", object1, typeof(GameObject), true) as GameObject;
+            if (GUILayout.Button("Select", GUILayout.Width(60)))
+            {
+                EditorGUIUtility.PingObject(Selection.activeGameObject = object1);
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            object2 = EditorGUILayout.ObjectField("Object 2", object2, typeof(GameObject), true) as GameObject;
+            if (GUILayout.Button("Select", GUILayout.Width(60)))
+            {
+                EditorGUIUtility.PingObject(Selection.activeGameObject = object2);
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+
+            if (Selection.transforms.Length == 2)
+            {
+                if (GUILayout.Button("Assign Selected Objects"))
+                {
+                    object1 = Selection.transforms[0].gameObject;
+                    object2 = Selection.transforms[1].gameObject;
+                }
+            }
+            else
+            {
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Set Object 1"))
+                {
+                    object1 = selectedObject;
+                }
+
+                if (GUILayout.Button("Set Object 2"))
+                {
+                    object2 = selectedObject;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+
+            if (object1 != null && object2 != null)
+            {
+                Vector3 worldDelta = object1.transform.position - object2.transform.position;
+                Vector3 localDelta = object1.transform.InverseTransformDirection(worldDelta);
+
+                EditorGUI.BeginChangeCheck();
+                Vector3 newLocalDelta = EditorGUILayout.Vector3Field("Difference", localDelta);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Vector3 newWorldDelta = object1.transform.TransformDirection(newLocalDelta);
+                    Undo.RecordObject(object2.transform, "Move Object 2");
+                    object2.transform.position = object1.transform.position - newWorldDelta;
+                }
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Apply difference to Snap"))
+                {
+                    EditorSnapSettings.move = new Vector3(
+                        Mathf.Abs(localDelta.x),
+                        Mathf.Abs(localDelta.y),
+                        Mathf.Abs(localDelta.z)
+                    );
+                }
+                if (GUILayout.Button("Copy", GUILayout.Width(50)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = localDelta.Vector3ToString();
+                }
+                GUILayout.EndHorizontal();
+
+                if (GUILayout.Button(new GUIContent("Match Bounds Center", "Moves Object 1 so its Renderer bounds center matches Object 2")))
+                {
+                    MatchBoundsCenter(object1.GetComponent<Renderer>(), object2.GetComponent<Renderer>());
+                }
+            }
+
+            ParentSystem();
+        }
 
         void ParentSystem()
         {
