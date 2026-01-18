909c80f0db658b147777b5fb03db328058481e3d
jailx365 <battzSHOT11@gmail.com>
2026-01-18 08:00:27 +0800

    Update Chance.cs

--- Runtime/Scripts/Utilities/Chance.cs
+++ Runtime/Scripts/Utilities/Chance.cs
@@ -19,5 +19,30 @@ namespace CrossingLears
 
         public static T RandomEnum<T>() where T : struct, IComparable, IFormattable, IConvertible 
             => ((T[])Enum.GetValues(typeof(T))).GetRandomFromList();
+
+        public static T RandomEnum<T>(params float[] weights) where T : struct, IComparable, IFormattable, IConvertible
+        {
+            T[] values = (T[])Enum.GetValues(typeof(T));
+
+            if (weights == null || weights.Length != values.Length)
+                throw new ArgumentException("Weights must match enum length.");
+
+            float total = 0f;
+            for (int i = 0; i < weights.Length; i++)
+                total += weights[i];
+
+            float random = Random.Range(0f, total);
+            float sum = 0f;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += weights[i];
+                if (random <= sum)
+                    return values[i];
+            }
+
+            return values[values.Length - 1];
+        }
+
     }
 }
