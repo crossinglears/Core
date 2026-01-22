5fcc7f5227a419ad7e819045f1aaf425c7afce54
jailx365 <battzSHOT11@gmail.com>
2026-01-22 11:01:46 +0800

    Update TextureRendererTab.cs

--- Editor/CL Window/Windows/TextureRendererTab.cs
+++ Editor/CL Window/Windows/TextureRendererTab.cs
@@ -51,110 +51,121 @@ namespace CrossingLearsEditor
         private HashSet<KeyCode> pressedKeys = new HashSet<KeyCode>();
 
         public override void DrawContent()
+{
+    CL_Window.current.Repaint();
+
+    camera = (Camera)EditorGUILayout.ObjectField("Camera", camera, typeof(Camera), true);
+    folder = (DefaultAsset)EditorGUILayout.ObjectField("Folder", folder, typeof(DefaultAsset), false);
+
+    GUILayout.Space(20f);
+
+    EditorGUILayout.BeginHorizontal();
+    fileName = EditorGUILayout.TextField("Name", fileName);
+    fileType = (FileType)EditorGUILayout.EnumPopup(fileType, GUILayout.Width(70));
+    EditorGUILayout.EndHorizontal();
+
+    outputResolution = EditorGUILayout.Vector2Field("Output Resolution", outputResolution);
+
+    EditorGUILayout.BeginHorizontal();
+    cameraMoveSpeed = EditorGUILayout.FloatField("Camera MoveSpeed", cameraMoveSpeed);
+    cameraLookSpeed = EditorGUILayout.FloatField("Camera LookSpeed", cameraLookSpeed);
+    EditorGUILayout.EndHorizontal();
+
+    GUILayout.Space(20f);
+
+    Rect previewRect = GUILayoutUtility.GetRect(
+        0f,
+        float.MaxValue,
+        0f,
+        float.MaxValue,
+        GUILayout.ExpandWidth(true),
+        GUILayout.ExpandHeight(true)
+    );
+
+    if (camera != null)
+    {
+        float targetAspect = outputResolution.x / outputResolution.y;
+        float rectAspect = previewRect.width / previewRect.height;
+
+        Rect fittedRect = previewRect;
+
+        if (targetAspect > rectAspect)
         {
-            CL_Window.current.Repaint();
-
-            camera = (Camera)EditorGUILayout.ObjectField("Camera", camera, typeof(Camera), true);
-            folder = (DefaultAsset)EditorGUILayout.ObjectField("Folder", folder, typeof(DefaultAsset), false);
-
-            GUILayout.Space(20f);
-
-            EditorGUILayout.BeginHorizontal();
-            fileName = EditorGUILayout.TextField("Name", fileName);
-            fileType = (FileType)EditorGUILayout.EnumPopup(fileType);
-            EditorGUILayout.EndHorizontal();
-
-            outputResolution = EditorGUILayout.Vector2Field("Output Resolution", outputResolution);
-
-            
-            EditorGUILayout.BeginHorizontal();
-            cameraMoveSpeed = EditorGUILayout.FloatField("Camera MoveSpeed", cameraMoveSpeed);
-            cameraLookSpeed = EditorGUILayout.FloatField("Camera LookSpeed", cameraLookSpeed);
-            EditorGUILayout.EndHorizontal();
-
-            GUILayout.Space(20f);
-
-            Rect previewRect = GUILayoutUtility.GetRect(
-                0f,
-                float.MaxValue,
-                0f,
-                float.MaxValue,
-                GUILayout.ExpandWidth(true),
-                GUILayout.ExpandHeight(true)
-            );
-
-            if (camera != null)
-            {
-                float targetAspect = outputResolution.x / outputResolution.y;
-                float rectAspect = previewRect.width / previewRect.height;
-
-                Rect fittedRect = previewRect;
-
-                if (targetAspect > rectAspect)
-                {
-                    float fittedHeight = previewRect.width / targetAspect;
-                    fittedRect.y += (previewRect.height - fittedHeight) * 0.5f;
-                    fittedRect.height = fittedHeight;
-                }
-                else
-                {
-                    float fittedWidth = previewRect.height * targetAspect;
-                    fittedRect.x += (previewRect.width - fittedWidth) * 0.5f;
-                    fittedRect.width = fittedWidth;
-                }
-
-                HandleCameraInput(fittedRect);
-
-                int previewWidth = Mathf.RoundToInt(outputResolution.x);
-                int previewHeight = Mathf.RoundToInt(outputResolution.y);
-
-                RenderTexture previewRT = RenderTexture.GetTemporary(previewWidth, previewHeight, 16);
-                camera.targetTexture = previewRT;
-                camera.Render();
-                camera.targetTexture = null;
-
-                EditorGUI.DrawPreviewTexture(fittedRect, previewRT, null, ScaleMode.ScaleToFit);
-                RenderTexture.ReleaseTemporary(previewRT);
-            }
-            else
-            {
-                EditorGUI.DrawRect(previewRect, Color.black);
-            }
-
-            GUILayout.Space(10f);
-
-            if (!GUILayout.Button("Render"))
-                return;
-
-            if (camera == null || folder == null)
-                return;
-
-            int outputWidth = Mathf.RoundToInt(outputResolution.x);
-            int outputHeight = Mathf.RoundToInt(outputResolution.y);
-
-            RenderTexture output = new RenderTexture(outputWidth, outputHeight, 24);
-            Texture2D texture = new Texture2D(outputWidth, outputHeight, TextureFormat.RGB24, false);
-
-            camera.targetTexture = output;
-            camera.Render();
-
-            RenderTexture.active = output;
-            texture.ReadPixels(new Rect(0f, 0f, outputWidth, outputHeight), 0, 0);
-            texture.Apply();
-
-            camera.targetTexture = null;
-            RenderTexture.active = null;
-
-            byte[] bytes = fileType == FileType.PNG ? texture.EncodeToPNG() : texture.EncodeToJPG();
-            string extension = fileType == FileType.PNG ? ".png" : ".jpg";
-
-            string path = AssetDatabase.GetAssetPath(folder);
-            System.IO.File.WriteAllBytes(path + "/" + fileName + extension, bytes);
-            AssetDatabase.Refresh();
-
-            Object.DestroyImmediate(output);
-            Object.DestroyImmediate(texture);
+            float fittedHeight = previewRect.width / targetAspect;
+            fittedRect.y += (previewRect.height - fittedHeight) * 0.5f;
+            fittedRect.height = fittedHeight;
         }
+        else
+        {
+            float fittedWidth = previewRect.height * targetAspect;
+            fittedRect.x += (previewRect.width - fittedWidth) * 0.5f;
+            fittedRect.width = fittedWidth;
+        }
+
+        HandleCameraInput(fittedRect);
+
+        int previewWidth = Mathf.RoundToInt(outputResolution.x);
+        int previewHeight = Mathf.RoundToInt(outputResolution.y);
+
+        RenderTexture previewRT = RenderTexture.GetTemporary(previewWidth, previewHeight, 16);
+        camera.targetTexture = previewRT;
+        camera.Render();
+        camera.targetTexture = null;
+
+        EditorGUI.DrawPreviewTexture(fittedRect, previewRT, null, ScaleMode.ScaleToFit);
+        RenderTexture.ReleaseTemporary(previewRT);
+    }
+    else
+    {
+        EditorGUI.DrawRect(previewRect, Color.black);
+    }
+
+    GUILayout.Space(10f);
+
+    if (!GUILayout.Button("Render"))
+        return;
+
+    if (camera == null || folder == null)
+        return;
+
+    int outputWidth = Mathf.RoundToInt(outputResolution.x);
+    int outputHeight = Mathf.RoundToInt(outputResolution.y);
+
+    RenderTexture output = new RenderTexture(outputWidth, outputHeight, 24);
+    Texture2D texture = new Texture2D(outputWidth, outputHeight, TextureFormat.RGB24, false);
+
+    camera.targetTexture = output;
+    camera.Render();
+
+    RenderTexture.active = output;
+    texture.ReadPixels(new Rect(0f, 0f, outputWidth, outputHeight), 0, 0);
+    texture.Apply();
+
+    camera.targetTexture = null;
+    RenderTexture.active = null;
+
+    byte[] bytes = fileType == FileType.PNG ? texture.EncodeToPNG() : texture.EncodeToJPG();
+    string extension = fileType == FileType.PNG ? ".png" : ".jpg";
+
+    string path = AssetDatabase.GetAssetPath(folder);
+    string finalName = fileName;
+    string fullPath = path + "/" + finalName + extension;
+
+    int index = 1;
+    while (System.IO.File.Exists(fullPath))
+    {
+        finalName = fileName + " (" + index + ")";
+        fullPath = path + "/" + finalName + extension;
+        index++;
+    }
+
+    System.IO.File.WriteAllBytes(fullPath, bytes);
+    AssetDatabase.Refresh();
+
+    Object.DestroyImmediate(output);
+    Object.DestroyImmediate(texture);
+}
+
 
         private void HandleCameraInput(Rect rect)
         {
