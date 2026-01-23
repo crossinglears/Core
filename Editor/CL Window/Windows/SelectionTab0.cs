94dbd635424e7f277777ed3ed8de7c1952c473ed
jailx365 <battzSHOT11@gmail.com>
2026-01-23 09:43:29 +0800

    Modified Selection.ParentSystem

--- Editor/CL Window/Windows/SelectionTab0.cs
+++ Editor/CL Window/Windows/SelectionTab0.cs
@@ -117,61 +117,6 @@ namespace CrossingLears.Editor
             }
         }
 
-        void ParentSystem()
-        {
-            GUILayout.Space(20);
-            ParentTransform = EditorGUILayout.ObjectField("Parent", ParentTransform, typeof(Transform), true) as Transform;
-
-            if (GUILayout.Button("Assign Parent"))
-            {
-                ParentTransform = selectedObject.transform;
-            }
-            if (GUILayout.Button("Assign Children"))
-            {
-                foreach (Transform item in Selection.transforms)
-                {
-                    Undo.SetTransformParent(item, ParentTransform, "Assign Children");
-                }
-            }
-
-            if (GUILayout.Button("Unparent"))
-            {
-                Transform[] targets = Selection.transforms;
-
-                void DoUnparent(Transform t)
-                {
-                    Undo.RecordObject(t, "Unparent");
-
-                    Transform parent = t.parent;
-
-                    if (parent == null)
-                    {
-                        t.SetParent(null);
-                        return;
-                    }
-
-                    Transform grand = parent.parent;
-                    int targetIndex = parent.GetSiblingIndex() + 1;
-
-                    if (grand == null)
-                    {
-                        t.SetParent(null);
-                        t.SetSiblingIndex(targetIndex);
-                        return;
-                    }
-
-                    t.SetParent(grand);
-                    t.SetSiblingIndex(targetIndex);
-                }
-
-                for (int i = 0; i < targets.Length; i++)
-                {
-                    DoUnparent(targets[i]);
-                }
-
-                EditorGUIUtility.PingObject(selectedObject);
-            }
-        }
 
         public static void MatchBoundsCenter(Renderer source, Renderer target)
         {
