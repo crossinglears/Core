414a26c41dd566be11fdb101b89dcc927851c234
jailx365 <battzSHOT11@gmail.com>
2026-06-14 06:30:55 +0800

    String Extensions

---
+++ Runtime/Scripts/Utilities/StringExtensions.cs
@@ -0,0 +1,187 @@
+using UnityEngine;
+
+namespace CrossingLears
+{
+    public static class StringExtensions
+    {
+        public static string Bold(this string input)
+        {
+            return $"<b>{input}</b>";
+        }
+
+        public static string Italic(this string input)
+        {
+            return $"<i>{input}</i>";
+        }
+
+        public static string Underline(this string input)
+        {
+            return $"<u>{input}</u>";
+        }
+
+        public static string Strikethrough(this string input)
+        {
+            return $"<s>{input}</s>";
+        }
+
+        public static string Color(this string input, Color color)
+        {
+            return $"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{input}</color>";
+        }
+
+        public static string Color(this string input, string color)
+        {
+            return $"<color={color}>{input}</color>";
+        }
+
+        public static string Size(this string input, int size)
+        {
+            return $"<size={size}>{input}</size>";
+        }
+
+        public static string Material(this string input, int materialIndex)
+        {
+            return $"<material={materialIndex}>{input}</material>";
+        }
+
+        public static string Sprite(int spriteIndex)
+        {
+            return $"<sprite={spriteIndex}>";
+        }
+
+        public static string Sprite(string spriteName)
+        {
+            return $"<sprite name=\"{spriteName}\">";
+        }
+
+        public static string FontWeight(this string input, string weight)
+        {
+            return $"<font-weight={weight}>{input}</font-weight>";
+        }
+
+        public static string Uppercase(this string input)
+        {
+            return $"<uppercase>{input}</uppercase>";
+        }
+
+        public static string Lowercase(this string input)
+        {
+            return $"<lowercase>{input}</lowercase>";
+        }
+
+        public static string SmallCaps(this string input)
+        {
+            return $"<smallcaps>{input}</smallcaps>";
+        }
+
+        public static string AllCaps(this string input)
+        {
+            return $"<allcaps>{input}</allcaps>";
+        }
+
+        public static string Superscript(this string input)
+        {
+            return $"<sup>{input}</sup>";
+        }
+
+        public static string Subscript(this string input)
+        {
+            return $"<sub>{input}</sub>";
+        }
+
+        public static string Mark(this string input, Color color)
+        {
+            return $"<mark=#{ColorUtility.ToHtmlStringRGBA(color)}>{input}</mark>";
+        }
+
+        public static string Alpha(this string input, float percentage)
+        {
+            byte alpha = (byte)Mathf.Clamp(Mathf.RoundToInt(percentage / 100f * 255f), 0, 255);
+            return $"<alpha=#{alpha:X2}>{input}";
+        }
+
+        public static string NoParse(this string input)
+        {
+            return $"<noparse>{input}</noparse>";
+        }
+
+        public static string Link(this string input, string id)
+        {
+            return $"<link=\"{id}\">{input}</link>";
+        }
+
+        public static string Indent(this string input, int pixels)
+        {
+            return $"<indent={pixels}>{input}</indent>";
+        }
+
+        public static string LineIndent(this string input, int pixels)
+        {
+            return $"<line-indent={pixels}>{input}</line-indent>";
+        }
+
+        public static string Margin(this string input, int pixels)
+        {
+            return $"<margin={pixels}>{input}</margin>";
+        }
+
+        public static string MarginLeft(this string input, int pixels)
+        {
+            return $"<margin-left={pixels}>{input}</margin-left>";
+        }
+
+        public static string MarginRight(this string input, int pixels)
+        {
+            return $"<margin-right={pixels}>{input}</margin-right>";
+        }
+
+        public static string Width(this string input, int pixels)
+        {
+            return $"<width={pixels}>{input}</width>";
+        }
+
+        public static string Monospace(this string input, int pixels)
+        {
+            return $"<mspace={pixels}>{input}</mspace>";
+        }
+
+        public static string CharacterSpace(this string input, int pixels)
+        {
+            return $"<cspace={pixels}>{input}</cspace>";
+        }
+
+        public static string LineHeight(this string input, int pixels)
+        {
+            return $"<line-height={pixels}>{input}</line-height>";
+        }
+
+        public static string Position(this string input, int pixels)
+        {
+            return $"<pos={pixels}>{input}</pos>";
+        }
+
+        public static string VerticalOffset(this string input, int pixels)
+        {
+            return $"<voffset={pixels}>{input}</voffset>";
+        }
+
+        public static string Rotate(this string input, int degrees)
+        {
+            return $"<rotate={degrees}>{input}</rotate>";
+        }
+
+        public enum TextAlignment
+        {
+            Left,
+            Center,
+            Right,
+            Justified,
+            Flush,
+            Geo
+        }
+        public static string Align(this string input, TextAlignment alignment)
+        {
+            return $"<align=\"{alignment}\">{input}</align>";
+        }
+    }
+}
